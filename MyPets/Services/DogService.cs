30356abfa165a37d181561371383cad0bcd27bb0
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-07 11:47:51 -0300

    feat: add dog service

---
+++ MyPets/Services/DogService.cs
@@ -0,0 +1,42 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyPets.Services
+{
+    internal class DogService
+    {
+        private const string DOG_API_URL = "https://dogapi.dog/api/v2";
+
+        public static RestResponse GetDogBreeds()
+        {
+            var client = new RestClient($"{DOG_API_URL}/breeds");
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            return response;
+        }
+
+        public class DogBreedsResponse
+        {
+            public List<DogBreed> data { get; set; }
+        }
+
+        public class DogBreed
+        {
+            public string id { get; set; }
+            public BreedAttributes attributes { get; set; }
+        }
+
+        public class BreedAttributes
+        {
+            public string name { get; set; }
+            public string description { get; set; }
+        }
+    }
+}
